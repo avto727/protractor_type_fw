e8f6e3ba95e63d5d38ea294dd72c1686aa32c542
avto727 <avto727@bk.ru>
2019-08-11 16:14:59 +0300

    elements in apart file+1

--- pages/fill_order_Page.js
+++ pages/fill_order_Page.js
@@ -1,15 +1,6 @@
 var basePage = require('./basePage.js');
 
 var LoginPage = function() {
-    this.dict = {
-        url: "/#/dashboard",
-        userInput:element(by.name('email')),
-        passInput:element(by.name('pwd')),
-        loginButton:$('.form-bttns.h4 > button'),
-        errorMessage:$('div.alert.alert-danger'),
-        login_email:$('#root > react-ui-view-adapter > div > div > div.Content-sc-1av4hia-0.eDWmeO > div > nav > div > div > button > span'),
-        pageLoaded: this.and(this.isVisible($('div#page')))
-        };
 
     this.loginAs = function(userObj) {
         this.login(userObj.username, userObj.password);
